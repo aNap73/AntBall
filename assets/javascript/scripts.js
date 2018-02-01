7fbc4d36d4677747e904c733c686b4347c266cbd
aNap73 <napolitano.anthony@yahoo.com>
2018-02-01 15:07:03 -0500

    Changed screen size and renamed some elements.

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -2,6 +2,8 @@ var balllft=0;
 var balltp=0;
 var balldeltalft=1;
 var balldeltatp=1;
+var scrnheight=280;
+var scrnwidth=640;
 function StartBall ()
 {
   Step();
@@ -17,17 +19,17 @@ function Step()
 function BallAI()
 {
   
-  if(balllft >= 640 | balllft < 0 )
+  if(balllft >= scrnwidth | balllft < 0 )
   {
     balldeltalft *= -1;
   }
-  if(balltp >= 480 | balltp < 0 )
+  if(balltp >= scrnheight | balltp < 0 )
   {
     balldeltatp *= -1;
   }
   balllft += balldeltalft;
   balltp += balldeltatp;
-  var Ball = document.getElementById('AIBall');
+  var Ball = document.getElementById("ball1");
   
     
   Ball.style = "left: " + balllft + "px; top: " + balltp + "px;";
